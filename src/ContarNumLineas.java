6ae0ceff3320d581f63a857f1e6bd77cd053c1f1
danymcbookair <dsmmgc@gmail.com>
2024-07-17 15:32:47 +0100

    añadidos ejercicios

---
+++ src/ContarNumLineas.java
@@ -0,0 +1,20 @@
+/*Escribamos un programa que lea 10líneas del teclado y muestre el número de líneas que eran números*/
+
+import java.util.Scanner;
+
+public class ContarNumLineas {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        for (int i = 0; i <10; i++) {
+
+            if (sc.hasNextInt()) {
+                count++;
+                sc.nextInt();
+            }
+        } System.out.println(count);
+    }
+}
