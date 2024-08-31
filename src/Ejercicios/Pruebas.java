0b3899d468a5876bb26e804b55a9a6863efda982
Dany3gs <dsmmgc@gmail.com>
2024-08-31 16:24:55 +0100

    hay alguien

--- src/Ejercicios/Pruebas.java
+++ src/Ejercicios/Pruebas.java
@@ -1,36 +1,35 @@
 package Ejercicios;
 
-/*Implemente el método main(String[]) , que imprime true en la consola si el elemento está contenido en la matriz pasada. de lo contrario falso.
-La matriz array no debe cambiar la disposición de sus elementos.
-
-Para encontrar un elemento en una matriz, necesita una búsqueda binaria. Para hacer esto, use el método static Arrays.binarySearch(int[], int) de la clase Arrays . El primer parámetro es pasar la matriz ordenada en la que buscar, y el segundo parámetro es el elemento que se buscará. El método Arrays.binarySearch(int[], int) devuelve el índice del elemento buscado, si dicho elemento está en la matriz.
-
-Para ordenar una matriz, puede utilizar el método Arrays.sort(int[]) .
-Al realizar la prueba, los valores de las variables de la clase Solution serán diferentes.
-Requisitos:
-
-    •
-    En el método main (String []), no cambie la disposición de los elementos de la matriz.
-    •
-    Al llamar al método main(String[]), muestre verdadero en la consola si el valor de la variable del elemento está contenido en la matriz.
-    •
-    Al llamar al método main(String[]), muestre false en la consola si el elemento pasado no está contenido en la matriz.
-    •
-    En el método main (String []), utilice la llamada al método Arrays.binarySearch (int [], int).*/
-
 
 import java.util.Arrays;
 
 public class Pruebas {
 
-    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int element = 5;
+    
+   
 
     public static void main(String[] args) {
         //escribe aquí tu código
-         int[] copy = Arrays.copyOf(array, array.length);
-        Arrays.sort(copy);  
-        int index = Arrays.binarySearch(copy, element);
-        System.out.println(index >= 0);
+
+
+            int[] numbers = {167, -2, 16, 99, 26, 92, 43, -234, 35, 80};
+            Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+            for (int i = numbers.length - 1; i > 0; i--) {
+                for (int j = 0; j < i; j++) {
+           /* Compare the elements in pairs.
+             If they are not in the right order,
+             then swap them */
+                    if (numbers[j] > numbers[j + 1]) {
+                        int tmp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = tmp;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(numbers));
+
+        }
     }
-}
--- src/Ejercicios/Tema7/HayAlguienArrayBuscado.java
+++ src/Ejercicios/Tema7/HayAlguienArrayBuscado.java
@@ -1,21 +1,16 @@
-package Ejercicios.Tema7;
+package Ejercicios;
 
-/*Implemente el método main(String[]) , que imprime true en la consola si el elemento está contenido en la matriz pasada,
-de lo contrario falso.
+/*Implemente el método main(String[]) , que imprime true en la consola si el elemento está contenido en la matriz pasada. de lo contrario falso.
 La matriz array no debe cambiar la disposición de sus elementos.
 
-Para encontrar un elemento en una matriz, necesita una búsqueda binaria. Para hacer esto, use el método static
-Arrays.binarySearch(int[], int) de la clase Arrays. El primer parámetro es pasar la matriz ordenada en la que buscar,
- y el segundo parámetro es el elemento que se buscará. El método Arrays.binarySearch(int[], int) devuelve el índice del
- elemento buscado, si dicho elemento está en la matriz.
+Para encontrar un elemento en una matriz, necesita una búsqueda binaria. Para hacer esto, use el método static Arrays.binarySearch(int[], int) de la clase Arrays . El primer parámetro es pasar la matriz ordenada en la que buscar, y el segundo parámetro es el elemento que se buscará. El método Arrays.binarySearch(int[], int) devuelve el índice del elemento buscado, si dicho elemento está en la matriz.
 
 Para ordenar una matriz, puede utilizar el método Arrays.sort(int[]) .
 Al realizar la prueba, los valores de las variables de la clase Solution serán diferentes.
 Requisitos:
 
     En el método main (String []), no cambie la disposición de los elementos de la matriz.
-    Al llamar al método main(String[]), muestre verdadero en la consola si el valor de la variable del elemento está
-    contenido en la matriz.
+    Al llamar al método main(String[]), muestre verdadero en la consola si el valor de la variable del elemento está contenido en la matriz.
     Al llamar al método main(String[]), muestre false en la consola si el elemento pasado no está contenido en la matriz.
     En el método main (String []), utilice la llamada al método Arrays.binarySearch (int [], int). */
 
@@ -29,7 +24,7 @@ public class HayAlguienArrayBuscado {
         public static void main(String[] args) {
 
         //primero hay que crear una array para copiar el original y ordenarlo
-        int[] copy = Arrays.copyOf(array, array.length);
+ø       int[] copy = Arrays.copyOf(array, array.length);
         Arrays.sort(copy);  
         //luego se crea variable que busque con Arrays.binarySearch() si se encuentra element entre los elementos
         int index = Arrays.binarySearch(copy, element);
