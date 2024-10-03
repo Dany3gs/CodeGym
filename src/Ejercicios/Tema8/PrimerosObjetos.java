8953ed93b6a661a95d193d51dd7d34be3b13e009
Dany3gs <dsmmgc@gmail.com>
2024-10-03 21:43:38 +0100

    nv 8 lecc 4

---
+++ src/Ejercicios/Tema8/PrimerosObjetos.java
@@ -0,0 +1,41 @@
+/*
+Usando el operador new en el método main(), cree:
+
+10 objetos String;
+5 objetos int[] (matrices de valores int);
+2 objetos Scanner.
+Requisitos:
+•	Se deben crear 10 objetos String en el método main().
+•	Se deben crear 5 objetos int[] en el método main().
+•	Se deben crear 2 objetos Scanner en el método main().*/
+package Ejercicios.Tema8;
+
+import java.util.Scanner;
+
+public class PrimerosObjetos {
+    public static void main(String[] args) {
+
+
+        //escribe aquí tu código
+        String objeto1 = new String("Objeto1");
+        String objeto2 = new String("Objeto2");
+        String objeto3 = new String("Objeto3");
+        String objeto4 = new String("Objeto4");
+        String objeto5 = new String("Objeto5");
+        String objeto6 = new String("Objeto6");
+        String objeto7 = new String("Objeto7");
+        String objeto8 = new String("Objeto8");
+        String objeto9 = new String("Objeto9");
+        String objeto10 = new String("Objeto10");
+
+        int[] matriz1 = new int[1];
+        int[] matriz2 = new int[2];
+        int[] matriz3 = new int[3];
+        int[] matriz4 = new int[4];
+        int[] matriz5 = new int[5];
+
+        Scanner sca = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+    }
+}
+//ok codegym
