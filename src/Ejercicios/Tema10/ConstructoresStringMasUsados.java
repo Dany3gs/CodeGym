42d217d91ef86f883d2bc1331d3cba45f797e25d
Dany3gs <dsmmgc@gmail.com>
2025-01-08 17:13:57 +0000

    docs: refactorizado explicaciones

--- src/Ejercicios/Tema10/ConstructoresStringMasUsados.java
+++ src/Ejercicios/Tema10/ConstructoresStringMasUsados.java
@@ -8,29 +8,29 @@ public class ConstructoresStringMasUsados {
 	public static void main(String[] args) {
 		
 		String nombre = "Métodos constructores más usados con la clase String.";
+		//-------------------------------------------------------------------------//
 		// int length
 		// Devuelve el número de caracteres de la cadena.
-		
 		System.out.println(nombre.length()); // output 53
 		
+		//-------------------------------------------------------------------------//
 		// boolean isEmpty()
 		// Comprueba si la cadena es una cadena vacía
-		
 		System.out.println(nombre.isEmpty()); // false
 		
+		//-------------------------------------------------------------------------//
 		// boolean isBlank()
 		// Comprueba que la cadena contiene "solo" caracteres de espacio en blanco: espacio, tabulador, nueva línea, etc.
-		
 		System.out.println(nombre.isBlank()); // false
 		
+		//-------------------------------------------------------------------------//
 		// char chasrAt(int index) -> hay que pasarle un valor int para ir a la posición de elemento
 		// Devuelve el carácter en la posición de índice en la cadena.
-		
 		System.out.println(nombre.charAt(3)); // output o (tercer elemento)
 		
+		//-------------------------------------------------------------------------//
 		// char[] toCharArray
 		// Devuelve una matriz de los caracteres (una copia) que componen la cadena
-		
 		char[] nombreComoArray = nombre.toCharArray();
 		System.out.println(nombreComoArray.length);
 		for(char c : nombreComoArray) {
@@ -38,9 +38,9 @@ public class ConstructoresStringMasUsados {
 		}
 		System.out.println(" ");
 		
+		//-------------------------------------------------------------------------//
 		// byte[] getBytes()
 		// Convierte una cadena en un conjunto de bytes y devuelve la matriz de bytes
-		
 		byte[] nombreBytes = nombre.getBytes();
 		System.out.println(nombreBytes.length);
 		for(byte b : nombreBytes) {
@@ -50,9 +50,9 @@ public class ConstructoresStringMasUsados {
 		System.out.println(" ");
 		System.out.println(Arrays.toString(nombreBytes)); // output array de valores binarios de cada char en charset UTF-8
 		
+		//-------------------------------------------------------------------------//
 		// String[] split(String regex)
 		// Divide una cadena en varias subcadenas según indiquemos en la expresión regular.
-		
 		String[] nombreSplit = nombre.split(" ");
 		System.out.println(nombreSplit.length); // output 8, las palabras que componen nombre
 		
@@ -67,6 +67,7 @@ public class ConstructoresStringMasUsados {
 			System.out.print(Arrays.toString(s.split(" ")));
 		}
 		
+		//-------------------------------------------------------------------------//
 		// String join(CharSequence delimiter, elements)
 		// Une varias subcadenas juntas
 		
@@ -79,6 +80,7 @@ public class ConstructoresStringMasUsados {
 		nombre = String.join(" ", nombre, nombreJoin);
 		System.out.println(nombre); // Output Métodos constructores más usados con la clase String. Esta frase es para unir a nombre
 		
+		//-------------------------------------------------------------------------//
 		// String intern()
 		// Pone una cadena en el string pool, se usa para gestionar mejor el uso de memoria.
 		// Si no encuentra una cadena igual, crea una copia exacta en el String Pool y devuelve la referencia a esta nueva copia
@@ -92,8 +94,7 @@ public class ConstructoresStringMasUsados {
 		System.out.println(s1 == s2); // false
 		System.out.println(s2 == s3); // true
 		
-		
-		//************************************************************
+		//-------------------------------------------------------------------------//
 		/*Solución 1: usar una matriz de caracteres
 		
 		Cree un objeto Scanner.
@@ -120,7 +121,7 @@ public class ConstructoresStringMasUsados {
 		String result = new String(chars);
 		System.out.println(result); // Output https:\\domain.com\about\reviews
 
-		console.close();
+		//console.close();
 	
 		/*Solución 2: — usando los métodos split()yjoin()
 		*Cree un objeto Scanner.
@@ -139,7 +140,7 @@ public class ConstructoresStringMasUsados {
 		String result1 = String.join("\\", array);
 		System.out.println(result1);// Output https:\\domain.com\about\reviews
 
-		console1.close();
+		//console1.close();
 		/*Solución 3: — usando el replace(char oldChar, char newChar)método
 
 		Cree un objeto Scanner.
